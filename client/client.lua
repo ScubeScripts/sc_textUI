f6aa9f9b932a9e0eceb2a2876a5d4a50ce1de000
Lumpii <104854776+Lumpii404@users.noreply.github.com>
2025-01-29 10:45:51 +0100

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,6 +1,5 @@
 local display = false
 
--- Funktion zum Anzeigen der TextUI
 function ShowTextUI(key, text)
     SendNUIMessage({
         type = "ui",
@@ -11,7 +10,6 @@ function ShowTextUI(key, text)
     display = true
 end
 
--- Funktion zum Verstecken der TextUI
 function HideTextUI()
     SendNUIMessage({
         type = "ui",
@@ -20,7 +18,6 @@ function HideTextUI()
     display = false
 end
 
--- Export-Funktionen für andere Client-Skripte
 exports("showUI", function(key, text)
     ShowTextUI(key, text)
 end)
@@ -29,7 +26,6 @@ exports("hideUI", function()
     HideTextUI()
 end)
 
--- Event, um die UI vom Server zu steuern
 RegisterNetEvent("sc_textUI:showUI", function(key, text)
     ShowTextUI(key, text)
 end)
