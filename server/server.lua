30fcf6e8e114290adf38222a365116a2663bbaaa
Lumpii <104854776+Lumpii404@users.noreply.github.com>
2025-01-29 10:46:06 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,9 +1,7 @@
--- Funktion, um die UI für einen spezifischen Spieler zu zeigen
 function ShowTextUIForPlayer(source, key, text)
     TriggerClientEvent("sc_textUI:showUI", source, key, text)
 end
 
--- Funktion, um die UI für einen spezifischen Spieler zu verstecken
 function HideTextUIForPlayer(source)
     TriggerClientEvent("sc_textUI:hideUI", source)
 end
